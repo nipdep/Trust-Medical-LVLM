1b6f38698d4d5e4e0048a723f4727b54499464a0
nipdep <nipun1deelaka@gmail.com>
2024-11-08 08:14:03 -0500

    [add] base methods

---
+++ src/methods/__init__.py
@@ -0,0 +1,5 @@
+from .unrelated_color import UnrelatedColorImage
+from .unrelated_nature import UnrelatedNatureImage
+from .unrelated_noise import UnrelatedNoiseImage
+from .related import RelatedTextEmbedImage, RelatedGeneratedImage
+from .adv import AdvGeneratedImage
