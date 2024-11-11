ad8ba08edb8088168dd73f0f03a88ec1f10c7328
nipdep <nipun1deelaka@gmail.com>
2024-11-11 17:14:02 -0500

    [update] mimic with binary data loader

--- src/datasets/mimic.py
+++ src/datasets/mimic.py
@@ -32,7 +32,10 @@ class Mimic(BaseDataset):
         base_dir = os.getcwd()  
         self.image_dir = "/home/pathin/safety_llm/Trust-Medical-LVLM/data/data/physionet.org/files/mimic-cxr-jpg/2.0.0/files"
         self.anno_dir = "/home/pathin/safety_llm/Trust-Medical-LVLM/data/data/physionet.org/files/mimic-cxr-jpg/2.0.0/modified_mimic_factuality.json"
+        
         self.annotations = self.load_json(self.anno_dir)
+        if dataset_id not in ["mimic", "mimic-open-ended-factuality"]:
+            self.annotations = self.filter_json(self.annotations)
         # self.filter_json()
         self.build_dataset()    
 
@@ -40,18 +43,15 @@ class Mimic(BaseDataset):
     def load_json(self, path):
         with open(path, "r") as f:
             json_data = json.load(f)
-        
         return json_data
 
-        # TODO: adding the filtering
 
-    def filter_json(self):
+    def filter_json(self, json_data):
         new_json = []
-        for entry in self.json_data:
+        for entry in json_data:
             if entry["binAnswer"] in ["Yes", "No"]:
                 new_json.append(entry)
-        self.json_data = new_json
-        print(f"filtered for yes/no: {len(self.json_data)} data loaded" )
+        return new_json
 
     def build_dataset(self):
         dataset = []
@@ -65,12 +65,6 @@ class Mimic(BaseDataset):
         self.dataset = dataset
         print(f"{len(dataset)} data loaded")
 
-    # def prompt_template(self, info:Dict, prompt_tempte: Callable)->str:
-    #     return prompt_tempte(info["gender"], info["age"], info["race"], info["text"], info["pre_text"])
-    
-    # def get_chat_template(self, gender:str, age:int, race:str, text:str, pre_text:str)->str:
-    #     build_prompt = f"{text} {pre_text} {gender} {age} {race}"
-    #     return build_prompt
 
     def template_format(self, anno:Dict)-> ImageTxtSample:
         image_path_ = os.path.join(self.image_dir, anno["image"])
@@ -93,13 +87,13 @@ class Mimic(BaseDataset):
             target_ = anno["answer"]
         elif self.dataset_id == "mimic-binary":
             text_ = self.mimic_binary_template.format(question=text_)
-            target_ = anno["binAnswer"]
+            target_ = 1 if anno["binAnswer"]=="Yes" else 0
         elif self.dataset_id == "mimic-binary-factuality":
             text_ = self.mimic_binary_factuality_template.format(race=race_, gender=gender_, question=text_)
-            target_ = anno["binAnswer"]
+            target_ = 1 if anno["binAnswer"]=="yes" else 0
         elif self.dataset_id == "mimic-open-ended-factuality":
             text_ = self.mimic_binary_factuality_template.format(race=race_, gender=gender_, question=text_)
-            target_ = anno["Answer"]
+            target_ = anno["answer"]
 
         return ImageTxtSample(image_path=image_path_, text=text_, target=target_, extra=extra_)
     
@@ -112,7 +106,6 @@ class Mimic(BaseDataset):
     #     print(f"filtered for yes/no: {len(self.dataset)} data loaded" )
             
     def __getitem__(self, index: int) -> Tuple[ImageTxtSample, str]:
-    
         if self.method_hook:
             item =  self.method_hook.run(self.dataset[index])
         item =  self.dataset[index]
